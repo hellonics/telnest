07f768fe42368b7e1cd7a67c5f0897745a510743
hellonics <angganicholas93@gmail.com>
2023-02-26 18:41:37 +0700

    Template

---
+++ resources/views/city/index.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+
+@section('title', 'City -o- Telnest')
+
+@section('main)
+    <main id="main" class="main">
+        <h1>Ini adalah halaman Kota</h1>
+    </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CityController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('city.index');
 });
+
+Route::get('/city', [CityController::class, 'index']);
+
+Route::get('/city', [CityController::class, 'create']);
